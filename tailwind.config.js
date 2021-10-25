a2693057ef825d576ce3aa8612c491d04ba8af8c
Melvin Dionisio <75345253+melvindionisio@users.noreply.github.com>
2021-10-24 17:10:24 -0800

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./build/**/*.html", "./build/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
